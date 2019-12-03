32915bed6953d5ba6138c267353329a5fb4ac817
Ihor Martyniuk <devmartyniuk@gmail.com>
2019-12-03 20:53:57 +0200

    Improve dashbord activity

---
+++ app/src/main/java/com/itea/practice/fundamentals/task/PingDashboardActivity.java
@@ -0,0 +1,18 @@
+package com.itea.practice.fundamentals.task;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itea.practice.components.R;
+
+public class PingDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_ping_dashboard);
+    }
+}
