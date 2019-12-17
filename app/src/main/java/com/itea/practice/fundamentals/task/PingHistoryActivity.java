c57277c72ad69e9115d42ffeec68359cfd94e972
Ihor Martyniuk <devmartyniuk@gmail.com>
2019-12-17 21:16:48 +0200

    Improve ping content provider

---
+++ app/src/main/java/com/itea/practice/fundamentals/task/PingHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.itea.practice.fundamentals.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PingHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ping_history);
+    }
+}
---
+++ app/src/main/java/com/itea/practice/fundamentals/task/PingHistoryProvider.java
@@ -0,0 +1,6 @@
+package com.itea.practice.fundamentals.task;
+
+import android.content.ContentProvider;
+
+public class PingHistoryProvider extends ContentProvider {
+}
